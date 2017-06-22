33c6c7a6c4cc90249310240fafaf4586553dad4a
arthur <rtpro1@gmail.com>
2017-06-22 17:40:58 +0300

    Fixing site build script

--- build_site.sh
+++ build_site.sh
@@ -3,7 +3,7 @@
 WORKDIR=/tmp/incubator-ariatosca-site
 rm -rf $WORKDIR
 mkdir -p $WORKDIR
-bundle exec jekyll build --destination $WORKDIR $WORKDIR
+bundle exec jekyll build --destination $WORKDIR 
 git checkout asf-site
 git clean -f -d
 git pull origin asf-site
