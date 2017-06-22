7c9b68b284926bef41d5e1c24f6ff9769a30be2c
arthur <rtpro1@gmail.com>
2017-06-22 16:53:15 +0300

    Updating site build

---
+++ _site/build_site.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WORKDIR=/tmp/incubator-ariatosca-site
+rm -rf $WORKDIR
+mkdir -p $WORKDIR
+bundle exec jekyll build --destination $WORKDIR $WORKDIR
+git checkout asf-site
+git clean -f -d
+git pull origin asf-site
+rm -rf *
+cp -a $WORKDIR/* .
+git add .
+git commit -m "Automatic Site Publish by Buildbot"
+git push origin asf-site
