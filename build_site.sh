fab861a694f78fac96eb9a901bcbccecb2f22c70
jenkins <builds@apache.org>
2017-06-22 14:31:44 +0000

    Automatic Site Publish by Buildbot

--- build_site.sh
+++ build_site.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
 WORKDIR=/tmp/incubator-ariatosca-site
 rm -rf $WORKDIR
 mkdir -p $WORKDIR
-gem install bundler
+export RUBY_PATH=~/shared/.rvm
+export GEM_HOME=${RUBY_PATH}/gems
+curl -sSL https://get.rvm.io | bash -s -- --path ${RUBY_PATH}
+mkdir -p ${GEM_HOME}/gems
+gem install  --install-dir ${GEM_HOME} bundler
+export PATH=${GEM_HOME}/bin:$PATH
+bundle install --path ${GEM_HOME}
+bundle
 bundle exec jekyll build --destination $WORKDIR $WORKDIR
 git checkout asf-site
 git clean -f -d
